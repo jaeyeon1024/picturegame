b3e9e8c392071472d2c6bb5b7d0dba10013766b3
JaeYeon <jaeyeon2001@gmail.com>
2023-08-09 13:54:18 +0900

    08-09

--- Main.py
+++ Main.py
@@ -1,37 +1,27 @@
+import os
 import cv2
 import random
-import matplotlib.pyplot as plt
 import numpy as np
-import sys
-input = sys.stdin.readline
 
 row_size = 512 # 사각형 크기
 col_size = 512
-window_size = 4 # 블러 크기
+window_size = 2 # 블러 크기
 error_range = 30 # 오차범위
+save_path = r"C:\Labs\picture_game\static\css\img\saves.jpg"
+__image_path = r"static\css\img\meercat.jpg"
+__instance = None
 
 #이미지 설정
 def img_resize(img,background):
-    img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.resize(img, dsize=(row_size, col_size), interpolation=cv2.INTER_AREA)
     background = cv2.resize(background, dsize=(row_size, col_size), interpolation=cv2.INTER_AREA)
     return img,background
 
-# 픽셀 체크
-def check_pexels(img_data:list, rgb:list) -> bool:
-    check = True
-    if img_data[0]-error_range > rgb[0] or img_data[0]+error_range < rgb[0]:
-        check = False
-    elif img_data[1]-error_range > rgb[1] or img_data[1]+error_range < rgb[1]:
-        check = False
-    elif img_data[2]-error_range > rgb[2] or img_data[2]+error_range < rgb[2]:
-        check = False
-    return check
 # 스머징 구현
 def Smudge_Tool(img):
     for i in range(img.shape[0]):
         for j in range(1,img.shape[1]):
-            random_num = random.randrange(3)
+            random_num = random.randrange(2)
             img[i,j],img[i-random_num,j-random_num] = img[i-random_num,j-random_num],img[i,j]
     return img
 
@@ -49,50 +39,55 @@ def blurring_img(img):
             img[i][j] = list(pixel_mean)
     return img
 
+# 픽셀 체크
+def check_pexels(img_data:list, rgb:list) -> bool:
+    check = True
+    if img_data[0]-error_range > rgb[0] or img_data[0]+error_range < rgb[0]:
+        check = False
+    elif img_data[1]-error_range > rgb[1] or img_data[1]+error_range < rgb[1]:
+        check = False
+    elif img_data[2]-error_range > rgb[2] or img_data[2]+error_range < rgb[2]:
+        check = False
+    return check
+
 #픽셀 출력
 def print_pexels(img,background,rgb):
     for i in range(img.shape[0]-1):
         for j in range(img.shape[1]-1):
             if check_pexels(list(img[i][j]),rgb):
                 background[i][j] = list(img[i][j])
-    return background
+    cv2.imwrite(save_path, background)
+    return 
 
-if __name__ == '__main__':
-
-    img = cv2.imread(r"img\train.png")
-    background = cv2.imread(r"img\white_background.png")
+#
+def __create():
+    img = cv2.imread(__image_path)
+    background = cv2.imread(r"static\css\img\white_background.jpg")
 
     img, background = img_resize(img,background)
     img = Smudge_Tool(img)
     img = blurring_img(img)
+    return [img, background]
 
-    plt.imshow(img)
-    plt.show()
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-    while(True):
-        rgb = list(map(int, input().split()))
-        background = print_pexels(img,background,rgb)
-        plt.imshow(background)
-        plt.show()
-        cv2.waitKey()
-        cv2.destroyAllWindows()
+def update(path: str):
+    if not os.path.exists(path):
+        raise FileNotFoundError()
+    if not os.path.isfile(path):
+        raise RuntimeError(f"This is not file ({path})")
+    __image_path = path
+
+def create():
+    __instance = __create()
+    return __instance
 
 
+def get():
+    if __instance is None:
+        raise RuntimeError()
+    return __instance
 
-# print("back : ",background.shape)
-# print("Size info : ",img.shape)
-# print("Heght :" ,img.shape[0])
-# print("colum :" ,img.shape[1])
-# print("info type : ",type(img.shape[0]))
-
-# print(type(img))
-# print(img)
-
-#cv2.waitKey()
-
-#cv2.destroyAllWindows()
-
+def reset(background):
+    cv2.imwrite(save_path, background)
 
 # 케니 엣지
 
--- sever.py
+++ sever.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template, request
-import Main.py
+import Main
+
 
 app = Flask(__name__)
+img , background = Main.create()
+Main.reset(background)
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html" , image_file='css/img/white_background.jpg')
 
 
-@app.route('/result',methods = ['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-      result = request.form
-      return Main() [int(result[0]),int(result[1]),int(result[2])]
-
-
-
+@app.route('/color',methods = ['POST', 'GET'])
+def color():
+    if request.method == 'POST':
+        global background
+        result = request.form
+        be_rgb = result['rgb']
+        rgb = [int(be_rgb[5:],base=16) ,int(be_rgb[3:5],base=16),int(be_rgb[1:3],base=16)]
+        Main.print_pexels(img,background,rgb)
+        return render_template("index.html" , image_file='css/img/saves.jpg')
 
 app.run(port=5001,debug=True)
